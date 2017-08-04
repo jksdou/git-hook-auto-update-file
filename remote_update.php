588943b85a19511e8e67c65cd197c13a7e8f9ce1
唐钰豆豆 <doudoudzj@sina.com>
2017-08-04 23:07:40 +0800

    add Token check

--- remote_update.php
+++ remote_update.php
@@ -1,40 +1,46 @@
 <?php
 error_reporting(1);
 header("Content-type: text/html; charset=utf-8");
+$token = 'lkagkw34o5ukndljg03';
+if (empty($_GET['token']) || $_GET['token'] !== $token) {
+  exit('Error Token');
+}
 $logfile  = 'log.json';
-
 $data = file_get_contents('php://input'); // print_r(urldecode($data)."\n");
 $json = json_decode($data, true);
-if ($f = file_put_contents($logfile, $data."\n", FILE_APPEND)) {
-  echo 'log wrote successed'."\n";
+$github_raw = 'https://raw.githubusercontent.com';
+$github_user = 'doudoudzj';
+$github_repo = 'random-picture';
+$github_branch = 'master';
+$github_uri = $github_raw.'/'.$github_user.'/'.$github_repo.'/'.$github_branch;
+
+if ($f = file_put_contents($logfile, time().':'.$data."\n", FILE_APPEND)) {
+  echo 'log successed'."\n";
 }
 if (empty($json['commits'])) {
   echo "<br />\n";
   exit('Error Request');
 }
+
 for($i = 0; $i < count($json['commits']); $i++) {
   if (!empty($json['commits'][$i]['modified'])) {
-    for($j = 0; $j < count($json['commits'][$i]['modified']); $j++) {
-      if (!empty($json['commits'][$i]['modified'][$j])) {
-        echo download(
-          "https://raw.githubusercontent.com/doudoudzj/random-picture/master/".$json['commits'][$i]['modified'][$j]."?t=".time(),
-          "/",
-          $json['commits'][$i]['modified'][$j]);
-        if ($f = file_put_contents($logfile, "modified:".$json['commits'][$i]['modified'][$j]."\n", FILE_APPEND)) {
-          echo 'log wrote successed'."\n";
+    $modified = $json['commits'][$i]['modified'];
+    for($j = 0; $j < count($modified); $j++) {
+      if (!empty($modified[$j])) {
+        echo download($github_uri.'/'.$modified[$j].'?t='.time(), '/', $modified[$j])."\n";
+        if ($f = file_put_contents($logfile, time().': modified,'.$modified[$j]."\n", FILE_APPEND)) {
+          echo time().': log successed, modified, '.$modified[$j]."\n";
         }
       }
     }
   }
   if (!empty($json['commits'][$i]['added'])) {
-    for($j = 0; $j < count($json['commits'][$i]['added']); $j++) {
-      if (!empty($json['commits'][$i]['added'][$j])) {
-        echo download(
-          "https://raw.githubusercontent.com/doudoudzj/random-picture/master/".$json['commits'][$i]['added'][$j]."?t=".time(),
-          "/",
-          $json['commits'][$i]['added'][$j]);
-        if ($f = file_put_contents($logfile, "added:".$json['commits'][$i]['added'][$j]."\n", FILE_APPEND)) {
-          echo 'log wrote successed'."\n";
+    $added = $json['commits'][$i]['added'];
+    for($j = 0; $j < count($added); $j++) {
+      if (!empty($added[$j])) {
+        echo download($github_uri.'/'.$added[$j].'?t='.time(), '/', $added[$j])."\n";
+        if ($f = file_put_contents($logfile, time().': added,'.$added[$j]."\n", FILE_APPEND)) {
+          echo time().': log successed, added, '.$added[$j]."\n";
         }
       }
     }
@@ -48,13 +54,13 @@ function download($url, $dir, $filename=''){
   $ext = strrchr($url, '.');
   $dir = realpath($dir);
   $filename = $dir . $filename;
-  //开始捕捉
+
   ob_start();
   readfile($url);
-  $img = ob_get_contents();
+  $file = ob_get_contents();
   ob_end_clean();
   $fp2 = fopen($filename , "w+");
-  fwrite($fp2, $img);
+  fwrite($fp2, $file);
   fclose($fp2);
   return $filename;
 }
